ec178c1fc52764ef77021eeefd71aa7e89aa67b9
Avi Punes <avi@papayaglobal.com>
2019-04-28 17:25:17 +0300

    revert export position

--- src/index.js
+++ src/index.js
@@ -5,6 +5,46 @@ import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 
+import Alert from "Common/Alert";
+import Attachment from "Common/Attachment";
+import Button from "Common/Button";
+import Calendar from "Common/Calendar";
+import CheckBox from "Common/Checkbox";
+import Dropdown from "Common/Dropdown";
+import Link from "Common/HyperLink";
+import Label from "Common/Label";
+import Message from "Common/Message";
+import PopOver from "Common/PopOver";
+import RadioButton from "Common/Radio";
+import ReportRow from "Common/ReportRow";
+import SelectBox from "Common/Select";
+import Spinner from "Common/Spinner";
+import Switch from "Common/Switch";
+import TextArea from "Common/TextArea";
+import TextInput from "Common/TextInput";
+import ToolTip from "Common/Tooltip";
+
+export {
+    Alert,
+    Attachment,
+    Button,
+    Calendar,
+    CheckBox,
+    Dropdown,
+    Link,
+    Label,
+    Message,
+    PopOver,
+    RadioButton,
+    ReportRow,
+    SelectBox,
+    Spinner,
+    Switch,
+    TextArea,
+    TextInput,
+    ToolTip
+};
+
 ReactDOM.render(<App />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
