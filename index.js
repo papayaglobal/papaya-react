76fbcf7efc762ba4f529fafa8541814db78dfe50
Avi Punes <avi@papayaglobal.com>
2019-04-28 17:07:07 +0300

    move export to project root

---
+++ index.js
@@ -0,0 +1,39 @@
+import Alert from "./src/Common/Alert";
+import Attachment from "./src/Common/Attachment";
+import Button from "./src/Common/Button";
+import Calendar from "./src/Common/Calendar";
+import CheckBox from "./src/Common/Checkbox";
+import Dropdown from "./src/Common/Dropdown";
+import Link from "./src/Common/HyperLink";
+import Label from "./src/Common/Label";
+import Message from "./src/Common/Message";
+import PopOver from "./src/Common/PopOver";
+import RadioButton from "./src/Common/Radio";
+import ReportRow from "./src/Common/ReportRow";
+import SelectBox from "./src/Common/Select";
+import Spinner from "./src/Common/Spinner";
+import Switch from "./src/Common/Switch";
+import TextArea from "./src/Common/TextArea";
+import TextInput from "./src/Common/TextInput";
+import ToolTip from "./src/Common/Tooltip";
+
+export {
+    Alert,
+    Attachment,
+    Button,
+    Calendar,
+    CheckBox,
+    Dropdown,
+    Link,
+    Label,
+    Message,
+    PopOver,
+    RadioButton,
+    ReportRow,
+    SelectBox,
+    Spinner,
+    Switch,
+    TextArea,
+    TextInput,
+    ToolTip
+};
