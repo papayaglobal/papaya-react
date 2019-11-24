eef95253647b4085af47efbf047075527d786cd3
idanhe <idanhe@papayaglobal.com>
2019-11-24 13:28:16 +0200

    fix attachments and added lodash

--- rollup.config.js
+++ rollup.config.js
@@ -9,61 +9,61 @@ import svgr from '@svgr/rollup'
 import pkg from './package.json'
 
 export default {
-  input: 'src/index.js',
-  output: [
-    {
-      file: pkg.main,
-      format: 'cjs',
-      sourcemap: true
-    },
-    {
-      file: pkg.module,
-      format: 'es',
-      sourcemap: true
-    }
-  ],
-  plugins: [
-    external(),
-    postcss({
-      modules: true
-    }),
-    url(),
-    svgr(),
-    babel({
-      exclude: 'node_modules/**'
-    }),
-    resolve(),
-    commonjs({
-      include: [
-          'node_modules/**',
-          /node_modules\/prop-types/,
-        ],
-        exclude: [
-          'node_modules/process-es6/**',
-        ],
-        namedExports: {
-        'node_modules/react/index.js': [
-          'Children',
-          'PureComponent',
-          'cloneElement',
-          'createContext',
-          'Component',
-          'createElement',
-          'isValidElement',
-        ],
-        'node_modules/react-dom/index.js': [
-          'findDOMNode',
-          'render',
-          'hydrate',
-          'createPortal'
-        ],
-        'node_modules/react-is/index.js': [
-          'isElement',
-          'isValidElementType',
-          'ForwardRef'
-        ]
-      }
-    })
-  ],
-  external: ['react', 'react-dom', 'prop-types', 'styled-components']
+    input: 'src/index.js',
+    output: [
+        {
+            file: pkg.main,
+            format: 'cjs',
+            sourcemap: true
+        },
+        {
+            file: pkg.module,
+            format: 'es',
+            sourcemap: true
+        }
+    ],
+    plugins: [
+        external(),
+        postcss({
+            modules: true
+        }),
+        url(),
+        svgr(),
+        babel({
+            exclude: 'node_modules/**'
+        }),
+        resolve(),
+        commonjs({
+            include: [
+                'node_modules/**',
+                /node_modules\/prop-types/,
+            ],
+            exclude: [
+                'node_modules/process-es6/**',
+            ],
+            namedExports: {
+                'node_modules/react/index.js': [
+                    'Children',
+                    'PureComponent',
+                    'cloneElement',
+                    'createContext',
+                    'Component',
+                    'createElement',
+                    'isValidElement',
+                ],
+                'node_modules/react-dom/index.js': [
+                    'findDOMNode',
+                    'render',
+                    'hydrate',
+                    'createPortal'
+                ],
+                'node_modules/react-is/index.js': [
+                    'isElement',
+                    'isValidElementType',
+                    'ForwardRef'
+                ]
+            }
+        })
+    ],
+    external: ['react', 'react-dom', 'prop-types', 'styled-components', 'lodash']
 }
--- src/stories/paymentRow.stories.js
+++ src/stories/paymentRow.stories.js
@@ -126,70 +126,111 @@ storiesOf("Payment Row", module)
         </div>
     ))
     .add("Real Data", () => (
-        <div className="app attachment">
-            <PaymentRow
-                attachments={object("attachments", [
-                    {
-                        "id": "34",
-                        "paymentId": "1926",
-                        "fileId": "101",
-                        "type": "PAYSLIP",
-                        "status": null,
-                        "isAudited": 0,
-                        "auditedBy": null,
-                        "createdAt": "2019-11-20T09:39:50.000Z",
-                        "updatedAt": "2019-11-20T09:39:50.000Z",
-                        "file": {
-                            "id": "101",
-                            "title": "guy_resume.pdf",
-                            "name": "guy_resume.pdf",
-                            "key": "dev/project/38/worker/368/payment/0455e8b6-907d-4e42-9061-1c099d40309f",
-                            "type": "worker_payment",
-                            "uploadDate": "2019-11-20",
-                            "uploadedByUserId": "1",
-                            "directUrl": "http://customer-1.dev.papayaglobal.com:8080/api/v1/file/101/link?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjQ3LCJrZXkiOiIxMDEiLCJpYXQiOjE1NzQ1ODkzMTcsImV4cCI6MTU3NTE5NDExN30.PIck31UCEe1rxtM2t1plPff49RvNXKteOVa6ZYoQlaA",
-                            "createdAt": "2019-11-20T09:39:47.000Z",
-                            "updatedAt": "2019-11-20T09:39:47.000Z",
-                            "__typename": "File"
+        <>
+            <div className="app attachment">
+                <PaymentRow
+                    attachments={object("attachments", [
+                        {
+                            "id": "34",
+                            "paymentId": "1926",
+                            "fileId": "101",
+                            "type": "PAYSLIP",
+                            "status": null,
+                            "isAudited": 0,
+                            "auditedBy": null,
+                            "createdAt": "2019-11-20T09:39:50.000Z",
+                            "updatedAt": "2019-11-20T09:39:50.000Z",
+                            "file": {
+                                "id": "101",
+                                "title": "guy_resume.pdf",
+                                "name": "guy_resume.pdf",
+                                "key": "dev/project/38/worker/368/payment/0455e8b6-907d-4e42-9061-1c099d40309f",
+                                "type": "worker_payment",
+                                "uploadDate": "2019-11-20",
+                                "uploadedByUserId": "1",
+                                "directUrl": "http://customer-1.dev.papayaglobal.com:8080/api/v1/file/101/link?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjQ3LCJrZXkiOiIxMDEiLCJpYXQiOjE1NzQ1ODkzMTcsImV4cCI6MTU3NTE5NDExN30.PIck31UCEe1rxtM2t1plPff49RvNXKteOVa6ZYoQlaA",
+                                "createdAt": "2019-11-20T09:39:47.000Z",
+                                "updatedAt": "2019-11-20T09:39:47.000Z",
+                                "__typename": "File"
+                            },
+                            "__typename": "WorkerPaymentDocument"
                         },
-                        "__typename": "WorkerPaymentDocument"
-                    },
-                    {
-                        "id": "35",
-                        "paymentId": "1927",
-                        "fileId": "102",
-                        "type": "PAYSLIP",
-                        "status": null,
-                        "isAudited": 0,
-                        "auditedBy": null,
-                        "createdAt": "2019-11-20T09:40:19.000Z",
-                        "updatedAt": "2019-11-20T09:40:19.000Z",
-                        "file": {
-                            "id": "102",
-                            "title": "guy_resume.pdf",
-                            "name": "guy_resume.pdf",
-                            "key": "dev/project/38/worker/368/payment/1d6b6c86-a7c1-490f-a597-c9c0062b0ba9",
-                            "type": "worker_payment",
-                            "uploadDate": "2019-11-20",
-                            "uploadedByUserId": "1",
-                            "directUrl": "http://customer-1.dev.papayaglobal.com:8080/api/v1/file/102/link?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjQ3LCJrZXkiOiIxMDIiLCJpYXQiOjE1NzQ1ODkzMTcsImV4cCI6MTU3NTE5NDExN30.QGYgTDP7bj6meelse6Fu7tXzkT8eIVR9FTMIsQneETI",
-                            "createdAt": "2019-11-20T09:40:17.000Z",
-                            "updatedAt": "2019-11-20T09:40:17.000Z",
-                            "__typename": "File"
-                        },
-                        "__typename": "WorkerPaymentDocument"
-                    }
-                ])}
-                amount={text("amount", "7,705.58")}
-                pendingTooltip="Report Row with multiple attachment"
-                dates={text("dates", "10 May 2019")}
-                reportedDate={text("reportedDate", "11 May 2019")}
-                actions={actions}
-                daysReported={text("daysReported", "1 Day")}
-                isNew={boolean("isNew", false)}
-                selectable={boolean("selectable", false)}
-            />
-        </div>
+                        {
+                            "id": "35",
+                            "paymentId": "1927",
+                            "fileId": "102",
+                            "type": "PAYSLIP",
+                            "status": null,
+                            "isAudited": 0,
+                            "auditedBy": null,
+                            "createdAt": "2019-11-20T09:40:19.000Z",
+                            "updatedAt": "2019-11-20T09:40:19.000Z",
+                            "file": {
+                                "id": "102",
+                                "title": "guy_resume.pdf",
+                                "name": "guy_resume.pdf",
+                                "key": "dev/project/38/worker/368/payment/1d6b6c86-a7c1-490f-a597-c9c0062b0ba9",
+                                "type": "worker_payment",
+                                "uploadDate": "2019-11-20",
+                                "uploadedByUserId": "1",
+                                "directUrl": "http://customer-1.dev.papayaglobal.com:8080/api/v1/file/102/link?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjQ3LCJrZXkiOiIxMDIiLCJpYXQiOjE1NzQ1ODkzMTcsImV4cCI6MTU3NTE5NDExN30.QGYgTDP7bj6meelse6Fu7tXzkT8eIVR9FTMIsQneETI",
+                                "createdAt": "2019-11-20T09:40:17.000Z",
+                                "updatedAt": "2019-11-20T09:40:17.000Z",
+                                "__typename": "File"
+                            },
+                            "__typename": "WorkerPaymentDocument"
+                        }
+                    ])}
+                    amount={text("amount", "7,705.58")}
+                    pendingTooltip="Report Row with multiple attachment"
+                    dates={text("dates", "10 May 2019")}
+                    reportedDate={text("reportedDate", "11 May 2019")}
+                    actions={actions}
+                    daysReported={text("daysReported", "1 Day")}
+                    isNew={boolean("isNew", false)}
+                    selectable={boolean("selectable", false)}
+                />
+            </div>
+            <div className="app attachment">
+                <PaymentRow
+                    attachments={object("attachments", [
+                        {
+                            "id": "34",
+                            "paymentId": "1926",
+                            "fileId": "101",
+                            "type": "PAYSLIP",
+                            "status": null,
+                            "isAudited": 0,
+                            "auditedBy": null,
+                            "createdAt": "2019-11-20T09:39:50.000Z",
+                            "updatedAt": "2019-11-20T09:39:50.000Z",
+                            "file": {
+                                "id": "101",
+                                "title": "guy_resume.pdf",
+                                "name": "guy_resume.pdf",
+                                "key": "dev/project/38/worker/368/payment/0455e8b6-907d-4e42-9061-1c099d40309f",
+                                "type": "worker_payment",
+                                "uploadDate": "2019-11-20",
+                                "uploadedByUserId": "1",
+                                "directUrl": "http://customer-1.dev.papayaglobal.com:8080/api/v1/file/101/link?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjQ3LCJrZXkiOiIxMDEiLCJpYXQiOjE1NzQ1ODkzMTcsImV4cCI6MTU3NTE5NDExN30.PIck31UCEe1rxtM2t1plPff49RvNXKteOVa6ZYoQlaA",
+                                "createdAt": "2019-11-20T09:39:47.000Z",
+                                "updatedAt": "2019-11-20T09:39:47.000Z",
+                                "__typename": "File"
+                            },
+                            "__typename": "WorkerPaymentDocument"
+                        }
+                    ])}
+                    amount={text("amount", "7,705.58")}
+                    pendingTooltip="Report Row with 1 attachment"
+                    dates={text("dates", "10 May 2019")}
+                    reportedDate={text("reportedDate", "11 May 2019")}
+                    actions={actions}
+                    daysReported={text("daysReported", "1 Day")}
+                    isNew={boolean("isNew", false)}
+                    selectable={boolean("selectable", false)}
+                />
+            </div>
+        </>
     ))
     .add("working checkbox", () => {
         let selectable = boolean("selectable", selectPaymentRow);
