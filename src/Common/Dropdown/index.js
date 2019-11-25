650e3cf50f5d41ae51ed0622f04ddabad18216e5
idanhe <idanhe@papayaglobal.com>
2019-11-25 09:55:45 +0200

    adding selectedItemIndex to dropdown

--- src/Common/Dropdown/index.js
+++ src/Common/Dropdown/index.js
@@ -6,68 +6,72 @@ import Button from "../Button";
 import dropIcon from "../../assets/icons/triangle.svg";
 
 class DropdownComponent extends React.Component {
-  state = {
-    toggleDisplay: false,
-    selectedItem: this.props.list[0].name
-  };
-  handleClickOutside = evt => {
-    this.setState({ toggleDisplay: false });
-  };
-  select = item => {
-    this.setState({
-      selectedItem: item.name,
-      toggleDisplay: !this.state.toggleDisplay
-    });
-    item.action();
-  };
-  renderListItems = () => {
-    let items = [];
-    for (let i = 0; i < this.props.list.length; i++) {
-      let item = this.props.list[i];
-      items.push(
-        <span className="dropdown-item" onClick={this.select.bind(null, item)} key={i}>
-          {item.name}
-        </span>
-      );
+    state = {
+        toggleDisplay: false,
+        selectedItem: null
+    };
+
+    componentDidMount() {
+        const {selectedItemIndex, list} = this.props;
+        this.setState({selectedItem: list[selectedItemIndex || 0].name})
     }
-    return items;
-  };
-  render() {
-    const { toggleDisplay, selectedItem } = this.state;
-    const { icon, className } = this.props;
-    return (
-      <div className={className}>
-        <Button
-          style={{ backgroundColor: "#ebebec", color: "#343949", fontSize: "1rem", height: 36 }}
-          onClick={() => this.setState({ toggleDisplay: !toggleDisplay })}
-        >
-          {icon ? (
-            <img src={icon} alt="Icon Only Button" />
-          ) : (
-            <React.Fragment>
-              <span className="btnText" style={{ paddingRight: 45 }}>
-                {selectedItem}
-              </span>
-              <img src={dropIcon} className="btnIcon" style={{ height: 6 }} alt="Dropdown Icon" />
-            </React.Fragment>
-          )}
-        </Button>
-        <div
-          className={`dropdown-menu ${toggleDisplay ? "show" : "hide"}`}
-          aria-labelledby="dropdownMenuButton"
-          x-placement="bottom-start"
-          style={{
-            position: "absolute",
-            willChange: "transform; top: 0px",
-            left: "10px",
-            transform: "translate3d(0px, 38px, 0px)"
-          }}
-        >
-          {this.renderListItems()}
-        </div>
-      </div>
-    );
-  }
+
+    handleClickOutside = evt => {
+        this.setState({toggleDisplay: false});
+    };
+
+    select = item => {
+        this.setState({
+            selectedItem: item.name,
+            toggleDisplay: !this.state.toggleDisplay
+        });
+        item.action();
+    };
+
+    renderListItems = () => {
+        let items = [];
+        for (let i = 0; i < this.props.list.length; i++) {
+            let item = this.props.list[i];
+            items.push(<span className="dropdown-item" onClick={this.select.bind(null, item)}
+                             key={i}>{item.name}</span>);
+        }
+        return items;
+    };
+
+    render() {
+        const {toggleDisplay, selectedItem} = this.state;
+        const {icon, className} = this.props;
+        return (
+            <div className={className}>
+                <Button
+                    style={{backgroundColor: "#ebebec", color: "#343949", fontSize: "1rem", height: 36}}
+                    onClick={() => this.setState({toggleDisplay: !toggleDisplay})}
+                >
+                    {icon ? (
+                        <img src={icon} alt="Icon Only Button"/>
+                    ) : (
+                        <React.Fragment>
+                            <span className="btnText" style={{paddingRight: 45}}>{selectedItem}</span>
+                            <img src={dropIcon} className="btnIcon" style={{height: 6}} alt="Dropdown Icon"/>
+                        </React.Fragment>
+                    )}
+                </Button>
+                <div
+                    className={`dropdown-menu ${toggleDisplay ? "show" : "hide"}`}
+                    aria-labelledby="dropdownMenuButton"
+                    x-placement="bottom-start"
+                    style={{
+                        position: "absolute",
+                        willChange: "transform; top: 0px",
+                        left: "10px",
+                        transform: "translate3d(0px, 38px, 0px)"
+                    }}
+                >
+                    {this.renderListItems()}
+                </div>
+            </div>
+        );
+    }
 }
 
 const Dropdown = styled(onClickOutside(DropdownComponent))`
--- src/stories/dropdown.stories.js
+++ src/stories/dropdown.stories.js
@@ -1,6 +1,6 @@
 import React from "react";
-import { storiesOf } from "@storybook/react";
-import { withKnobs, text, number } from "@storybook/addon-knobs";
+import {storiesOf} from "@storybook/react";
+import {withKnobs} from "@storybook/addon-knobs";
 
 import Dropdown from "../Common/Dropdown";
 import bars from "../assets/icons/bars.svg";
@@ -8,29 +8,34 @@ import bars from "../assets/icons/bars.svg";
 import "./stories.css";
 
 const colours = [
-  {
-    name: "By Month",
-    action: () => alert("Selected By Month")
-  },
-  {
-    name: "By Year",
-    action: () => alert("Selected By Year")
-  },
-  {
-    name: "By Day",
-    action: () => alert("Selected By Day")
-  }
+    {
+        name: "By Month",
+        action: () => alert("Selected By Month")
+    },
+    {
+        name: "By Year",
+        action: () => alert("Selected By Year")
+    },
+    {
+        name: "By Day",
+        action: () => alert("Selected By Day")
+    }
 ];
 
 storiesOf("Dropdown", module)
-  .addDecorator(withKnobs)
-  .add("Default", () => (
-    <div className="app spinner">
-      <Dropdown list={colours} />
-    </div>
-  ))
-  .add("Icon Only", () => (
-    <div className="app spinner">
-      <Dropdown list={colours} icon={bars} />
-    </div>
-  ));
+    .addDecorator(withKnobs)
+    .add("Default", () => (
+        <div className="app spinner">
+            <Dropdown list={colours}/>
+        </div>
+    ))
+    .add("Icon Only", () => (
+        <div className="app spinner">
+            <Dropdown list={colours} icon={bars}/>
+        </div>
+    ))
+    .add("With Selected Item", () => (
+        <div className="app spinner">
+            <Dropdown list={colours} selectedItemIndex={1}/>
+        </div>
+    ));
