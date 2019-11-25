07ae5c073feda7e59c7611289699c87c21890e3a
idanhe <idanhe@papayaglobal.com>
2019-11-25 13:53:08 +0200

    css changes for react select

--- src/Common/Select/index.js
+++ src/Common/Select/index.js
@@ -2,96 +2,106 @@ import React from "react";
 import Select from "react-select";
 import styled from "styled-components";
 
-const SelectComponent = ({ className, options, placeholder, onChange, isMulti, ...selectOptions }) => {
-  return (
-    <Select
-        {...selectOptions}
-        className={`${className} custom-select`}
-        options={options}
-        placeholder={placeholder}
-        onChange={onChange}
-        isMulti={isMulti}
-        styles={{
-        indicatorSeparator: (styles, { isFocused }) => {
-          return {
-            ...styles,
-            display: "none"
-          };
-        },
-        container: (styles, { isFocused }) => {
-          return {
-            ...styles,
-            backgroundColor: "#ffffff",
-            boxShadow: "0 1px 5px 0 rgba(0,0,0,0.10)",
-            padding: "0",
-            borderColor: "transparent",
-            borderBottomLeftRadius: isFocused ? 0 : 4,
-            borderBottomRightRadius: isFocused ? 0 : 4
-          };
-        },
-        control: (styles, { isFocused }) => {
-          return {
-            ...styles,
-            backgroundColor: "#ffffff",
-            border: isFocused ? 0 : 0,
-            borderBottomLeftRadius: isFocused ? 0 : 4,
-            borderBottomRightRadius: isFocused ? 0 : 4,
-            boxShadow: isFocused ? 0 : 0,
-            "&:hover": {
-              border: isFocused ? 0 : 0
-            }
-          };
-        },
-        option: (styles, { isFocused }) => {
-          return {
-            ...styles,
-            backgroundColor: isFocused ? "#F7F8FB" : "#ffffff",
-            borderColor: "transparent",
-            color: "#000000",
-            paddingLeft: 25,
-            paddingRight: 25
-          };
-        },
-        input: (styles, { isFocused }) => {
-          return {
-            ...styles,
-            paddingLeft: 5,
-            paddingRight: 5
-          };
-        },
-        singleValue: (styles, { isFocused }) => {
-          return {
-            ...styles,
-            paddingLeft: 15,
-            paddingRight: 15
-          };
-        },
-        placeholder: (styles, { isFocused }) => {
-          return {
-            ...styles,
-            fontSize: 14,
-            padding: "0 15px"
-          };
-        },
-        menu: (styles, { isFocused }) => {
-          return {
-            ...styles,
-            marginTop: 0,
-            borderTopLeftRadius: 0,
-            borderTopRightRadius: 0,
-            boxShadow: "0 4px 5px 0 rgba(0,0,0,0.10)"
-          };
-        },
-        menuList: (styles, { isFocused }) => {
-          return {
-            ...styles,
-            paddingTop: 0,
-            paddinRight: 25
-          };
-        }
-      }}
-    />
-  );
+const SelectComponent = ({className, options, placeholder, onChange, isMulti, ...selectOptions}) => {
+    return (
+        <Select
+            {...selectOptions}
+            className={`${className} custom-select`}
+            options={options}
+            placeholder={placeholder}
+            onChange={onChange}
+            isMulti={isMulti}
+            styles={{
+                indicatorSeparator: (styles, {isFocused}) => {
+                    return {
+                        ...styles,
+                        display: "none"
+                    };
+                },
+                container: (styles, {isFocused}) => {
+                    return {
+                        ...styles,
+                        backgroundColor: "#ffffff",
+                        // boxShadow: "0 1px 5px 0 rgba(0,0,0,0.10)",
+                        padding: "0",
+                        border: 0,
+                        borderRadius: "4px",
+                        // height: "",
+                        borderBottomLeftRadius: isFocused ? 0 : 4,
+                        borderBottomRightRadius: isFocused ? 0 : 4
+                    };
+                },
+                control: (styles, {isFocused}) => {
+                    return {
+                        ...styles,
+                        backgroundColor: "#ffffff",
+                        border: "1px solid #CCCCCC",
+                        borderBottomLeftRadius: isFocused ? 0 : 4,
+                        borderBottomRightRadius: isFocused ? 0 : 4,
+                        boxShadow: 0,
+                        "&:hover": {
+                            border: "1px solid #CCCCCC"
+                        }
+                    };
+                },
+                menu: (styles, {isFocused}) => {
+                    return {
+                        ...styles,
+                        marginTop: 0,
+                        borderTopLeftRadius: 4,
+                        borderTopRightRadius: 4,
+                        // border: "1px solid #CCCCCC",
+                        boxShadow: "0 1px 5px 0 rgba(0,0,0,0.10)"
+                    };
+                },
+                menuList: (styles, {isFocused}) => {
+                    return {
+                        ...styles,
+                        paddingTop: 0,
+                        paddinRight: 25
+                    };
+                },
+                option: (styles, {isFocused}) => {
+                    return {
+                        ...styles,
+                        backgroundColor: isFocused ? "#F7F8FB" : "#ffffff",
+                        borderColor: "transparent",
+                        fontFamily: "OpenSans",
+                        color: "#212529",
+                        fontSize: 16,
+                        paddingLeft: 25,
+                        paddingRight: 25,
+                        letterSpacing: 0
+                    };
+                },
+                input: (styles, {isFocused}) => {
+                    return {
+                        ...styles,
+                        paddingLeft: 5,
+                        paddingRight: 5
+                    };
+                },
+                singleValue: (styles, {isFocused}) => {
+                    return {
+                        ...styles,
+                        paddingLeft: 15,
+                        paddingRight: 15
+                    };
+                },
+                placeholder: (styles, {isFocused}) => {
+                    return {
+                        ...styles,
+                        fontFamily: "OpenSans",
+                        fontSize: 16,
+                        padding: "0 15px",
+                        color: "#B5B7BD",
+                        lineHeight: 24,
+                        letterSpacing: 0
+                    };
+                }
+            }}
+        />
+    );
 };
 
 const SelectBox = styled(SelectComponent)`
