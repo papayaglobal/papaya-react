dfdff82e150acec183bc1ba962bf162a4e77fb82
Itay Raz <itayraz@PAP-Itais-MacBook-Pro.local>
2020-03-25 18:11:17 +0200

    prop reactive

--- src/Common/Table/index.js
+++ src/Common/Table/index.js
@@ -1,17 +1,4 @@
-import {
-    filter,
-    get,
-    includes,
-    map,
-    omit,
-    orderBy,
-    slice,
-    update,
-    findIndex,
-    isEmpty,
-    isNil,
-    find
-} from "lodash";
+import { filter, get, includes, map, omit, orderBy, slice, update, findIndex, isEmpty, isNil, find } from "lodash";
 import React, { useEffect, useRef, useState } from "react";
 import styled from "styled-components";
 import { DARK3, DARK1 } from "../../Constants/colors";
@@ -45,7 +32,7 @@ export default function Table({
         };
     });
 
-    const customColumns = map(columns, column => {
+    const customColumns = map(columns, (column) => {
         return {
             ...column,
             sortOrder: null
@@ -58,6 +45,12 @@ export default function Table({
     const [rowCountState, setRowCountState] = useState(rowCountDefault);
     const [headerCheckboxState, setCheckboxState] = useState(false);
     const paginRef = useRef();
+
+    useEffect(() => {
+        if (!isEqual(customData, customDataState)) {
+            setCustomDataState(customData);
+        }
+    }, [data]);
 
     useEffect(() => {
         if (!isNil(onLazyLoad)) {
@@ -78,10 +71,7 @@ export default function Table({
             return;
         }
 
-        const sortedColumn = find(
-            customColumnState,
-            column => !isNil(column.sortOrder)
-        );
+        const sortedColumn = find(customColumnState, (column) => !isNil(column.sortOrder));
         const slicedData = await onLazyLoad({
             first: firstRowIndex,
             rowCount: rowCountState,
@@ -105,18 +95,8 @@ export default function Table({
             const isSortable = isNil(notSortable) || !notSortable;
 
             return (
-                <TableText
-                    key={`header-table-text-${headerIdx}`}
-                    flex={flex}
-                    header
-                >
-                    <span
-                        onClick={() =>
-                            isSortable && sortColumn(column, headerIdx)
-                        }
-                    >
-                        {output}
-                    </span>
+                <TableText key={`header-table-text-${headerIdx}`} flex={flex} header>
+                    <span onClick={() => isSortable && sortColumn(column, headerIdx)}>{output}</span>
                     <SortArrowContainer sortorder={sortOrder}>
                         <TableSortArrowIcon />
                     </SortArrowContainer>
@@ -127,12 +107,9 @@ export default function Table({
 
     const renderBody = () => {
         let rowsToShow = getRowsToShow();
-        return map(rowsToShow, row => {
+        return map(rowsToShow, (row) => {
             return (
-                <TableRowContainer
-                    key={`table-row-${row.rowIndex}`}
-                    isExpanded={row.isExpanded}
-                >
+                <TableRowContainer key={`table-row-${row.rowIndex}`} isExpanded={row.isExpanded}>
                     <TableRow isExpanded={row.isExpanded}>
                         {!isNil(row.isSelected) && (
                             <div>
@@ -143,20 +120,14 @@ export default function Table({
                             </div>
                         )}
                         {expandable && (
-                            <ExpandArrow
-                                onClick={() => expandRow(row.rowIndex, row)}
-                                isExpanded={row.isExpanded}
-                            >
+                            <ExpandArrow onClick={() => expandRow(row.rowIndex, row)} isExpanded={row.isExpanded}>
                                 <TableArrow></TableArrow>
                             </ExpandArrow>
                         )}
                         {map(customColumnState, (column, colKey) => {
                             const { colId, flex } = column;
                             return (
-                                <TableText
-                                    key={`body-table-text-${colKey}`}
-                                    flex={flex}
-                                >
+                                <TableText key={`body-table-text-${colKey}`} flex={flex}>
                                     {get(row, colId)}
                                 </TableText>
                             );
@@ -164,19 +135,12 @@ export default function Table({
                         {defaultSideMenu && renderSideMenu(row)}
                     </TableRow>
                     {expandable && (
-                        <ExpandRowContent
-                            isExpanded={row.isExpanded}
-                            ref={row.expandRowEl}
-                        >
+                        <ExpandRowContent isExpanded={row.isExpanded} ref={row.expandRowEl}>
                             {row.expandContent ? (
                                 row.expandContent
                             ) : (
                                 <SpinnerContainer>
-                                    <Spinner
-                                        width='34px'
-                                        height='34px'
-                                        color={DARK3}
-                                    />
+                                    <Spinner width="34px" height="34px" color={DARK3} />
                                 </SpinnerContainer>
                             )}
                         </ExpandRowContent>
@@ -189,7 +153,7 @@ export default function Table({
     const lazyRenderExpandContent = async (row, rowIndex) => {
         let currentData = customDataState;
         const newContent = await lazyExpand(row);
-        const index = currentData.findIndex(d => d.rowIndex === rowIndex);
+        const index = currentData.findIndex((d) => d.rowIndex === rowIndex);
         update(currentData, `[${index}].expandContent`, () => newContent);
 
         setCustomDataState([...currentData]);
@@ -202,11 +166,7 @@ export default function Table({
 
         let rowsToShow;
         if (rowCountState) {
-            rowsToShow = slice(
-                customDataState,
-                firstRowIndex,
-                firstRowIndex + rowCountState
-            );
+            rowsToShow = slice(customDataState, firstRowIndex, firstRowIndex + rowCountState);
         } else {
             rowsToShow = customDataState;
         }
@@ -214,12 +174,12 @@ export default function Table({
         return rowsToShow;
     };
 
-    const renderSideMenu = row => {
+    const renderSideMenu = (row) => {
         let list = [...defaultSideMenu];
         if (!isNil(row.sideMenuContent)) {
             list = row.sideMenuContent;
         }
-        list = map(list, sideMenu => ({
+        list = map(list, (sideMenu) => ({
             ...sideMenu,
             action: () => {
                 sideMenu.action(row);
@@ -229,11 +189,7 @@ export default function Table({
         return (
             <SideMenuContainer>
                 <StyledActions>
-                    <Dropdown
-                        list={list}
-                        icon={SideMenuIcon}
-                        buttonBackgroundColor={"transparent"}
-                    />
+                    <Dropdown list={list} icon={SideMenuIcon} buttonBackgroundColor={"transparent"} />
                 </StyledActions>
             </SideMenuContainer>
         );
@@ -241,17 +197,13 @@ export default function Table({
 
     const sortColumn = (column, headerIdx) => {
         let { sortOrder, colId } = column;
-        const updatedColumns = map(customColumnState, column => ({
+        const updatedColumns = map(customColumnState, (column) => ({
             ...column,
             sortOrder: null
         }));
 
         const updatedSortOrder = sortOrder === "asc" ? "desc" : "asc";
-        update(
-            updatedColumns,
-            `[${headerIdx}].sortOrder`,
-            () => updatedSortOrder
-        );
+        update(updatedColumns, `[${headerIdx}].sortOrder`, () => updatedSortOrder);
 
         setCustomColumnState([...updatedColumns]);
 
@@ -270,13 +222,10 @@ export default function Table({
         }
     };
 
-    const toggleCheckbox = rowIndex => {
+    const toggleCheckbox = (rowIndex) => {
         const updatedData = customDataState;
-        const index = findIndex(
-            updatedData,
-            item => item.rowIndex === rowIndex
-        );
-        update(updatedData, `[${index}].isSelected`, value => !value);
+        const index = findIndex(updatedData, (item) => item.rowIndex === rowIndex);
+        update(updatedData, `[${index}].isSelected`, (value) => !value);
         setCustomDataState([...updatedData]);
         if (onSelected) {
             onSelected(getSelected());
@@ -289,21 +238,17 @@ export default function Table({
     };
 
     const collapseRows = () => {
-        setCustomDataState(
-            map(customDataState, row => ({ ...row, isExpanded: false }))
-        );
+        setCustomDataState(map(customDataState, (row) => ({ ...row, isExpanded: false })));
     };
 
     const getSelected = () => {
-        let selectedRows = filter(customDataState, item => item.isSelected);
+        let selectedRows = filter(customDataState, (item) => item.isSelected);
 
-        return map(selectedRows, row =>
-            omit(row, ["isExpanded", "expandRowEl", "rowIndex"])
-        );
+        return map(selectedRows, (row) => omit(row, ["isExpanded", "expandRowEl", "rowIndex"]));
     };
 
-    const toggleAll = event => {
-        const updatedData = map(customDataState, item => {
+    const toggleAll = (event) => {
+        const updatedData = map(customDataState, (item) => {
             if (includes(getRowsToShow(), item)) {
                 item.isSelected = event.target.checked;
             }
@@ -320,20 +265,20 @@ export default function Table({
 
     const expandRow = (rowIndex, row) => {
         const updatedData = customDataState;
-        const index = updatedData.findIndex(d => d.rowIndex === rowIndex);
-        update(updatedData, `[${index}].isExpanded`, value => !value);
+        const index = updatedData.findIndex((d) => d.rowIndex === rowIndex);
+        update(updatedData, `[${index}].isExpanded`, (value) => !value);
         setCustomDataState([...updatedData]);
         if (lazyExpand && isNil(row.expandContent)) {
             lazyRenderExpandContent(row, rowIndex);
         }
     };
 
-    const changePage = page => {
+    const changePage = (page) => {
         collapseRows();
         setFirstRowIndex(page * rowCountState - rowCountState);
     };
 
-    const changeRowCount = rowCount => {
+    const changeRowCount = (rowCount) => {
         if (!rowCount) {
             return;
         }
@@ -346,10 +291,7 @@ export default function Table({
                 <TableRow header>
                     {!isNil(get(customDataState, "[0].isSelected")) && (
                         <div>
-                            <CheckBox
-                                checked={headerCheckboxState}
-                                onClick={toggleAll}
-                            />
+                            <CheckBox checked={headerCheckboxState} onClick={toggleAll} />
                         </div>
                     )}
                     {expandable && <ExpandArrow></ExpandArrow>}
@@ -361,11 +303,9 @@ export default function Table({
                     <Paging
                         ref={paginRef}
                         pageCount={Math.ceil(
-                            onLazyLoad
-                                ? totalRows / rowCountState
-                                : customDataState.length / rowCountState
+                            onLazyLoad ? totalRows / rowCountState : customDataState.length / rowCountState
                         )}
-                        onNumClick={page => changePage(page)}
+                        onNumClick={(page) => changePage(page)}
                         rowCountDefault={{
                             value: rowCountDefault,
                             label: rowCountDefault
@@ -390,27 +330,22 @@ const TableRow = styled.div`
     display: flex;
     align-items: center;
     padding: 10px;
-    color: ${props => (props.header ? `${DARK3}` : `${DARK1}`)};
-    box-shadow: ${props =>
-        props.header || props.isExpanded
-            ? "none"
-            : "0 1px 4px 0 rgba(0, 0, 0, 0.1)"};
-    border-radius: ${props => (props.header ? "none" : "4px")};
-    transition: ${props =>
-        props.isExpanded ? "none" : "box-shadow 1.2s ease-out"};
+    color: ${(props) => (props.header ? `${DARK3}` : `${DARK1}`)};
+    box-shadow: ${(props) => (props.header || props.isExpanded ? "none" : "0 1px 4px 0 rgba(0, 0, 0, 0.1)")};
+    border-radius: ${(props) => (props.header ? "none" : "4px")};
+    transition: ${(props) => (props.isExpanded ? "none" : "box-shadow 1.2s ease-out")};
 `;
 
 const TableRowContainer = styled.div`
     display: flex;
     flex-direction: column;
-    box-shadow: ${props =>
-        props.isExpanded ? "0 1px 4px 0 rgba(0, 0, 0, 0.1)" : "none"};
+    box-shadow: ${(props) => (props.isExpanded ? "0 1px 4px 0 rgba(0, 0, 0, 0.1)" : "none")};
     transition: box-shadow 0.5s ease-out;
     margin-bottom: 7px;
 `;
 
 const TableText = styled.div`
-    flex: ${props => props.flex};
+    flex: ${(props) => props.flex};
     font-weight: ${({ header }) => (header ? "bold" : "regular")};
     span {
         &:hover {
@@ -453,8 +388,7 @@ const SortArrowContainer = styled.span`
     svg {
         transition: transform 0.5s, height 0.1s;
         height: ${({ sortorder }) => (sortorder ? "5px" : "0px")};
-        transform: ${({ sortorder }) =>
-            sortorder === "desc" ? "rotate(0deg)" : "rotate(-180deg)"};
+        transform: ${({ sortorder }) => (sortorder === "desc" ? "rotate(0deg)" : "rotate(-180deg)")};
     }
 `;
 
