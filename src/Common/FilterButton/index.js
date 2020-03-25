36d42ea2968294e2a5fb6ea492e5cddc136ec4c7
Itay Raz <itayraz@PAP-Itais-MacBook-Pro.local>
2020-03-25 10:47:45 +0200

    prettier settings

--- src/Common/FilterButton/index.js
+++ src/Common/FilterButton/index.js
@@ -3,37 +3,29 @@ import styled from "styled-components";
 import { BRIGHT4, DARK1, ACCENT1 } from "../../Constants/colors";
 
 export default function FilterButton({ text, active, selected, onClick }) {
-  const handleClick = () => {
-    onClick();
-  };
+    const handleClick = () => {
+        onClick();
+    };
 
-  return (
-    <StyledFilterButton
-      active={active}
-      selected={selected}
-      onClick={handleClick}
-    >
-      {text}
-    </StyledFilterButton>
-  );
+    return (
+        <StyledFilterButton active={active} selected={selected} onClick={handleClick}>
+            {text}
+        </StyledFilterButton>
+    );
 }
 
 const StyledFilterButton = styled.div`
-  border: ${({ active, selected }) =>
-    active
-      ? `1px solid ${ACCENT1}`
-      : selected
-      ? `1px solid ${DARK1}`
-      : `1px solid ${BRIGHT4}`};
-  border-radius: 4px;
-  background: none;
-  padding: 8px 16px;
-  color: ${({ active }) => (active ? `${ACCENT1}` : `${DARK1}`)};
-  font-weight: ${({ selected }) => (selected ? "600" : "regular")};
-  user-select: none;
-  &:hover {
-    cursor: pointer;
-    border: 1px solid ${ACCENT1};
-    color: ${ACCENT1};
-  }
+    border: ${({ active, selected }) =>
+        active ? `1px solid ${ACCENT1}` : selected ? `1px solid ${DARK1}` : `1px solid ${BRIGHT4}`};
+    border-radius: 4px;
+    background: none;
+    padding: 8px 16px;
+    color: ${({ active }) => (active ? `${ACCENT1}` : `${DARK1}`)};
+    font-weight: ${({ selected }) => (selected ? "600" : "regular")};
+    user-select: none;
+    &:hover {
+        cursor: pointer;
+        border: 1px solid ${ACCENT1};
+        color: ${ACCENT1};
+    }
 `;
