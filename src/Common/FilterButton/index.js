6ac2040a3cec05b1f9bd5e5073675fb1a9acc596
Itay Raz <itayraz@PAP-Itais-MBP.lan>
2020-03-26 10:27:34 +0200

    filter button size change

--- src/Common/FilterButton/index.js
+++ src/Common/FilterButton/index.js
@@ -15,11 +15,15 @@ export default function FilterButton({ text, active, selected, onClick }) {
 }
 
 const StyledFilterButton = styled.div`
+    display: flex;
+    align-items: center;
     border: ${({ active, selected }) =>
         active ? `1px solid ${ACCENT1}` : selected ? `1px solid ${DARK1}` : `1px solid ${BRIGHT4}`};
     border-radius: 4px;
     background: none;
-    padding: 8px 16px;
+    padding-left: 16px;
+    padding-right: 16px;
+    height: 36px;
     color: ${({ active }) => (active ? `${ACCENT1}` : `${DARK1}`)};
     font-weight: ${({ selected }) => (selected ? "600" : "regular")};
     user-select: none;
